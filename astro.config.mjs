6ff5385956b727f1457d2367160710a5ae8c169c
YiYue <dyx060312@163.com>
2026-08-04 01:36:59 +0800

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { starlightThemeObsidianConfigSchema } from 'starlight-theme-obsidian/config';
+import starlightThemeObsidian from "starlight-theme-obsidian";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://notes.yiyuemeow.com",
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Ciallo~',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/YiYue345' }],
+			defaultLocale: "root",
+			locales: {
+				root: {
+					label: "简体中文",
+					lang: "zh-CN"
+				}
+			},
+			plugins: [
+				starlightThemeObsidian({
+					graph: false,
+					backlinks: false,
+				}),
+			],
 			sidebar: [
 				{
 					label: 'Guides',
